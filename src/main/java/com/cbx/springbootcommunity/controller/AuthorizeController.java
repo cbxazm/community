0557ebb957ee5df64023a80a81c121125c6ebc0c
cbxa <895001956@qq.com>
2019-11-15 15:10:12 +0800

    进一步完善登录

--- src/main/java/com/cbx/springbootcommunity/controller/AuthorizeController.java
+++ src/main/java/com/cbx/springbootcommunity/controller/AuthorizeController.java
@@ -1,8 +1,10 @@
 package com.cbx.springbootcommunity.controller;
 
 import com.cbx.springbootcommunity.dto.AccessTokenDto;
+import com.cbx.springbootcommunity.dto.GithubUser;
 import com.cbx.springbootcommunity.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,17 +13,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
     @GetMapping("/callback")
        public String callback(@RequestParam(name = "code") String code,@RequestParam(name = "state") String state){
 
         AccessTokenDto accessTokenDto = new AccessTokenDto();
-          accessTokenDto.setClient_id("d0c1d996ad5ffb3b9cf5");
-          accessTokenDto.setCode(code);
-          accessTokenDto.setClient_secret("bae90bbcaf31dc9a5cf7fcc7109f518354ee67bf");
-          accessTokenDto.setRedirect_uri("http://localhost:8887/callback");
-          accessTokenDto.setState(state);
-        githubProvider.getAccessToken(accessTokenDto);
-           return "index";
+          accessTokenDto.setClient_id(clientId);
+          accessTokenDto.setRedirect_uri(redirectUri);
+        accessTokenDto.setState(state);
+        accessTokenDto.setCode(code);
+        accessTokenDto.setClient_secret(clientSecret);
+        String accessToken = githubProvider.getAccessToken(accessTokenDto);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user);
+        return "index";
        }
 
 }
--- src/main/java/com/cbx/springbootcommunity/provider/GithubProvider.java
+++ src/main/java/com/cbx/springbootcommunity/provider/GithubProvider.java
@@ -20,28 +20,51 @@ public class GithubProvider {
                             .build();
                     try (Response response = client.newCall(request).execute()) {
                         String string = response.body().string();
-                        System.out.println(string);
-                        return string;
-                    } catch (IOException e) {
+                        System.out.println(string);//access_token=40227727e2ba54a22f2c08eb73d6ba81fd5162ca&scope=user&token_type=bearer
+                        /**
+                         *     进行分割，拿到acces_token的值
+
+                         */
+                        String[] split = string.split("&");
+                        String tokenStr = split[0];
+                        String token = tokenStr.split("=")[1];
+                        return token;
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
               return null;
             }
-            public GithubUser getUser(String accessToken){
-                OkHttpClient client = new OkHttpClient();
-                Request request = new Request.Builder()
-                        .url("https://api.github.com/user?access_token="+accessToken)
-                        .build();
-
-                try (Response response = client.newCall(request).execute()) {
-//                    return  response.body().string();
-                    String string=response.body().toString();
-//                    把string对象转换成为java的类对象
-                    GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
-                    return githubUser;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
+//            public GithubUser getUser(String accessToken){
+//                OkHttpClient client = new OkHttpClient();
+//                Request request = new Request.Builder()
+//                        .url("https://api.github.com/user?access_token="+accessToken)
+//                        .build();
+//
+//                try (Response response = client.newCall(request).execute()) {
+////                    return  response.body().string();
+//                    String string=response.body().toString();
+////                    把string对象转换成为java的类对象
+//                    GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+//                    System.out.println(githubUser);
+//                    return githubUser;
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                return null;
+//            }
+public GithubUser getUser(String accessToken) {
+    OkHttpClient client = new OkHttpClient();
+    Request request = new Request.Builder()
+            .url("https://api.github.com/user?access_token=" + accessToken)
+            .build();
+    try {
+        Response response = client.newCall(request).execute();
+        String string = response.body().string();
+        GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+        return githubUser;
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return null;
+}
 }
