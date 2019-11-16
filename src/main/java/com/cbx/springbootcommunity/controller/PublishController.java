ff7bbc9759b3c844e34c1ff9b3fae88d2de90908
cbxa <895001956@qq.com>
2019-11-16 16:16:39 +0800

    完成了发布文章的功能

---
+++ src/main/java/com/cbx/springbootcommunity/controller/PublishController.java
@@ -0,0 +1,67 @@
+package com.cbx.springbootcommunity.controller;
+
+import com.cbx.springbootcommunity.domain.Question;
+import com.cbx.springbootcommunity.domain.User;
+import com.cbx.springbootcommunity.mapper.QuestionMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class PublishController {
+    @Autowired
+    private QuestionMapper questionMapper;
+    //使用get方式则请求页面
+    @GetMapping("/publish")
+      public String publish(){
+           return "publish";
+      }
+      @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model
+      ){
+          /**
+           * 出现问题进行回显
+           */
+          model.addAttribute("title",title);
+          model.addAttribute("description",description);
+          model.addAttribute("tag",tag);
+        if (title==null||title==""){
+            model.addAttribute("error","标题不能为空");
+            return "publish";
+        }
+          if (description==null||description==""){
+              model.addAttribute("error","描述不能为空");
+              return "publish";
+          }
+          if (tag==null||tag==""){
+              model.addAttribute("error","标签不能为空");
+              return "publish";
+          }
+          User user = (User) request.getSession().getAttribute("user");
+          if(user==null){
+               model.addAttribute("error","用户未登录");
+               return "publish";
+          }
+          Question question = new Question();
+          question.setTitle(title);
+          question.setDescription(description);
+          question.setTag(tag);
+          question.setCreatorId(user.getId());
+          question.setGmtCreate(System.currentTimeMillis());
+          question.setGmtModified(question.getGmtCreate());
+          questionMapper.create(question);
+               return "redirect:/";
+      }
+
+}
---
+++ src/main/java/com/cbx/springbootcommunity/domain/Question.java
@@ -0,0 +1,110 @@
+package com.cbx.springbootcommunity.domain;
+
+public class Question {
+           private Integer id;
+           private String title;
+           private String description;
+           private Long gmtCreate;
+           private Long gmtModified;
+           private Integer commentCount;
+           private Integer ViewCount;
+           private Integer likeCount;
+           private String tag;
+           private Integer creatorId;
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", gmtCreate=" + gmtCreate +
+                ", gmtModified=" + gmtModified +
+                ", commentCount=" + commentCount +
+                ", ViewCount=" + ViewCount +
+                ", likeCount=" + likeCount +
+                ", tag='" + tag + '\'' +
+                ", creatorId=" + creatorId +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Long getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Long gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public Integer getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(Integer commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public Integer getViewCount() {
+        return ViewCount;
+    }
+
+    public void setViewCount(Integer viewCount) {
+        ViewCount = viewCount;
+    }
+
+    public Integer getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(Integer likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Integer getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(Integer creatorId) {
+        this.creatorId = creatorId;
+    }
+}
